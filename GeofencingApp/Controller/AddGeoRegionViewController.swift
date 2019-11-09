40b99c514033b4bb08f98aa78b62332873d489f5
Malika Arora <arora.malika9@gmail.com>
2019-11-09 12:50:34 +0530

    initial commit

---
+++ GeofencingApp/Controller/AddGeoRegionViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  GeofencingApp
+//
+//  Created by vinmac on 23/09/19.
+//  Copyright © 2019 vinmac. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+protocol AddGeoRegionDelegate {
+    func addGeoRegionViewController(coordinate: CLLocationCoordinate2D, radius: Double, identifier: String, note: String, eventType: EventType)
+}
+
+class AddGeoRegionViewController: UIViewController {
+    
+    // MARK: Private IBOutlets
+    @IBOutlet weak private var txtRadius: UITextField!
+    @IBOutlet weak private var txtNote: UITextView!
+    @IBOutlet weak private var mapView: MKMapView!
+    @IBOutlet weak private var btnEntry: UIButton!
+    @IBOutlet weak private var btnExit: UIButton!
+    
+    // MARK: Class properties
+    var delegate: AddGeoRegionDelegate?
+    private var firstResponder: UITextField?
+    private var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D() {
+        willSet(newValue) {
+            let allAnnotations = self.mapView.annotations
+            self.mapView.removeAnnotations(allAnnotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = newValue
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    // MARK: View controller life cycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.btnEntry.isSelected = true
+        self.btnExit.isSelected = false
+        self.mapView.showsUserLocation = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapViewTapped))
+        self.mapView.addGestureRecognizer(tapGesture)
+        self.txtRadius.keyboardType = .decimalPad
+        self.addToolbarToKeyboard()
+        self.txtNote.delegate = self
+        self.txtRadius.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.mapView.zoomToUserLocation()
+    }
+
+    // MARK: IBActions, ObjC methods
+    @IBAction private func btnCloseTapped(sender: AnyObject) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction private func btnEventTypeTapped(sender: UIButton) {
+        if sender.tag == 0 {
+            self.btnEntry.isSelected = true
+            self.btnEntry.setImage(#imageLiteral(resourceName: "filledRadioBtnImage"), for: .normal)
+            self.btnExit.setImage(#imageLiteral(resourceName: "unfilledRadioBtnImage"), for: .normal)
+            self.btnExit.isSelected = false
+        } else {
+            self.btnExit.isSelected = true
+            self.btnExit.setImage(#imageLiteral(resourceName: "filledRadioBtnImage"), for: .normal)
+            self.btnEntry.setImage(#imageLiteral(resourceName: "unfilledRadioBtnImage"), for: .normal)
+            self.btnEntry.isSelected = false
+        }
+    }
+    
+    @IBAction private func btnSaveTapped(sender: AnyObject) {
+        let coordinate = self.coordinate
+        let radius = Double(txtRadius.text!) ?? 0
+        let identifier = NSUUID().uuidString
+        let note = txtNote.text ?? ""
+        if note.isEmpty {
+            let alertVC = UIAlertController(title: "Warning", message: "Please provide a relevant note", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertVC.addAction(okAction)
+            self.present(alertVC, animated: true, completion: nil)
+        } else {
+            let entryType = self.btnEntry.isSelected ? EventType.onEntry : EventType.onExit
+            delegate?.addGeoRegionViewController(coordinate: coordinate, radius: radius, identifier: identifier, note: note, eventType: entryType)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction private func btnZoomToCurrentLocationTapped(sender: AnyObject) {
+        mapView.zoomToUserLocation()
+    }
+    
+    @objc private func mapViewTapped(gestureReconizer: UITapGestureRecognizer) {
+        let location = gestureReconizer.location(in: mapView)
+        self.coordinate = mapView.convert(location,toCoordinateFrom: mapView)
+    }
+    
+    @objc private func btnDoneTapped(sender: UIButton) {
+        self.firstResponder?.resignFirstResponder()
+    }
+    
+    // MARK: Class Private methods
+    private func addToolbarToKeyboard() {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        let btnDone = UIBarButtonItem(title: "done", style: .done, target: self, action: #selector(self.btnDoneTapped(sender:)))
+        toolbar.setItems([btnDone], animated: true)
+        self.txtRadius.inputAccessoryView = toolbar
+    }
+}
+
+extension AddGeoRegionViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
+        self.firstResponder = textField
+    }
+}
+
+extension AddGeoRegionViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+}
---
+++ GeofencingApp/Controller/GeoRegionsOnMapViewController.swift
@@ -0,0 +1,328 @@
+//
+//  GeoRegionsOnMapViewController.swift
+//  GeofencingApp
+//
+//  Created by vinmac on 23/09/19.
+//  Copyright © 2019 vinmac. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class GeoRegionsOnMapViewController: UIViewController {
+    
+    // MARK: Private IBOutlets
+    @IBOutlet weak private var mapView: MKMapView!
+    @IBOutlet weak private var segmentedControlBar: UISegmentedControl!
+    @IBOutlet weak private var lblRegionsCount: UILabel!
+        
+    // MARK: Actions methods
+    @IBAction private func btnAddRegionTapped(_ sender: UIButton) {
+        if CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            self.showAlert(title: "Warning", message: "This app requires access to Location. Please grant the Always Use permission in Settings")
+        } else {
+            if self.allGeoRegions.count >= 20 {
+                self.showAlert(title: "Warning", message: "Oops! Can't add more than 20 regions for region monitoring")
+            } else {
+                if let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddGeoRegionViewController") as? AddGeoRegionViewController {
+                    vc.delegate = self
+                    self.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+        }
+    }
+    
+    @IBAction private func zoomToCurrentLocation(sender: AnyObject) {
+        self.mapView.zoomToUserLocation()
+    }
+
+    @objc private func segmentedBarValueChanged(sender: UISegmentedControl) {
+        self.updateOnMap()
+    }
+    
+    // MARK: Class properties
+    var allGeoRegions: [GeoRegion] = []
+    var entryGeoRegions: [GeoRegion] = []
+    var exitGeoRegions: [GeoRegion] = []
+    let locationManager = CLLocationManager()
+    
+    // MARK: View controller life cycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.segmentedControlBar.addTarget(self, action: #selector(self.segmentedBarValueChanged), for: .valueChanged)
+        self.locationManager.delegate = self
+        self.checkLocationAuthorizationStatus()
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.loadAllGeoRegions()
+        self.filterGeoRegionsBasedOnEventType()
+        self.updateOnMap()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+    }
+    
+    // MARK: Private methods
+    private func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(okAction)
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    // MARK: Loading and saving functions
+    private func loadAllGeoRegions() {
+        self.allGeoRegions.removeAll()
+        self.allGeoRegions = GeoRegion.allGeoRegions()
+    }
+    
+    private func filterGeoRegionsBasedOnEventType() {
+        for geoRegion in self.allGeoRegions {
+            if geoRegion.eventType == .onEntry {
+                self.entryGeoRegions.append(geoRegion)
+            } else {
+                self.exitGeoRegions.append(geoRegion)
+            }
+        }
+    }
+    
+    private func saveAllGeoRegions() {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(self.allGeoRegions)
+            UserDefaults.standard.set(data, forKey: Utilities.savedItems)
+        } catch {
+            print("error encoding GeoRegions")
+        }
+    }
+    
+    private func updateOnMap() {
+        switch self.segmentedControlBar.selectedSegmentIndex {
+        case 0:
+            self.allGeoRegions.forEach({addOnMap(geoRegion: $0)})
+            self.lblRegionsCount.text = "Regions(\(self.allGeoRegions.count))"
+        case 1:
+            self.entryGeoRegions.forEach({addOnMap(geoRegion: $0)})
+            self.exitGeoRegions.forEach({removeFromMap(geoRegion: $0)})
+            self.lblRegionsCount.text = "Regions(\(self.entryGeoRegions.count))"
+        case 2:
+            self.exitGeoRegions.forEach({addOnMap(geoRegion: $0)})
+            self.entryGeoRegions.forEach({removeFromMap(geoRegion: $0)})
+            self.lblRegionsCount.text = "Regions(\(self.exitGeoRegions.count))"
+        default:
+            break
+        }
+        self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+    }
+    
+    private func addToGeoRegionArrayBasedOnEventType(geoRegion: GeoRegion) {
+        self.allGeoRegions.append(geoRegion)
+        if geoRegion.eventType == .onEntry {
+            self.entryGeoRegions.append(geoRegion)
+        } else {
+            self.exitGeoRegions.append(geoRegion)
+        }
+        self.addToMapBasedOnEventType(geoRegion: geoRegion)
+    }
+    
+    private func addToMapBasedOnEventType(geoRegion: GeoRegion) {
+        switch self.segmentedControlBar.selectedSegmentIndex {
+        case 0:
+            self.addOnMap(geoRegion: geoRegion)
+            self.lblRegionsCount.text = "Regions(\(self.allGeoRegions.count))"
+        case 1:
+            if geoRegion.eventType == .onEntry {
+                self.addOnMap(geoRegion: geoRegion)
+                self.lblRegionsCount.text = "Regions(\(self.entryGeoRegions.count))"
+            }
+        case 2:
+            if geoRegion.eventType == .onExit {
+                self.addOnMap(geoRegion: geoRegion)
+                self.lblRegionsCount.text = "Regions(\(self.exitGeoRegions.count))"
+            }
+        default:
+            break
+        }
+    }
+    
+    private func addOnMap(geoRegion: GeoRegion) {
+        self.mapView.addAnnotation(geoRegion)
+        addRadiusOverlay(forGeoRegion: geoRegion)
+    }
+    
+    private func removeFromGeoRegionArrayBasedOnEventType(geoRegion: GeoRegion) {
+        guard let index = allGeoRegions.firstIndex(of: geoRegion) else { return }
+        self.allGeoRegions.remove(at: index)
+        if geoRegion.eventType == .onEntry {
+            guard let i = entryGeoRegions.firstIndex(of: geoRegion) else { return }
+            self.entryGeoRegions.remove(at: i)
+        } else {
+            guard let i = exitGeoRegions.firstIndex(of: geoRegion) else { return }
+            self.exitGeoRegions.remove(at: i)
+        }
+        self.removeFromMapBasedOnEventType(geoRegion: geoRegion)
+    }
+    
+    private func removeFromMapBasedOnEventType(geoRegion: GeoRegion) {
+        switch self.segmentedControlBar.selectedSegmentIndex {
+        case 0:
+            removeFromMap(geoRegion: geoRegion)
+            self.lblRegionsCount.text = "Regions(\(self.allGeoRegions.count))"
+        case 1:
+            if geoRegion.eventType == .onEntry {
+                removeFromMap(geoRegion: geoRegion)
+                self.lblRegionsCount.text = "Regions(\(self.entryGeoRegions.count))"
+            }
+        case 2:
+            if geoRegion.eventType == .onExit {
+                removeFromMap(geoRegion: geoRegion)
+                self.lblRegionsCount.text = "Regions(\(self.exitGeoRegions.count))"
+            }
+        default:
+            break
+        }
+    }
+    
+    private func removeFromMap(geoRegion: GeoRegion) {
+        mapView.removeAnnotation(geoRegion)
+        removeRadiusOverlay(forGeoRegion: geoRegion)
+    }
+    
+    // MARK: Map overlay functions
+    private func addRadiusOverlay(forGeoRegion geoRegion: GeoRegion) {
+        mapView.addOverlay(MKCircle(center: geoRegion.coordinate, radius: geoRegion.radius))
+    }
+    
+    private func removeRadiusOverlay(forGeoRegion geoRegion: GeoRegion) {
+        guard let overlays = mapView?.overlays else { return }
+        for overlay in overlays {
+            guard let circleOverlay = overlay as? MKCircle else { continue }
+            let coordinate = circleOverlay.coordinate
+            if coordinate.latitude == geoRegion.coordinate.latitude && coordinate.longitude == geoRegion.coordinate.longitude && circleOverlay.radius == geoRegion.radius {
+                self.mapView.removeOverlay(circleOverlay)
+                break
+            }
+        }
+    }
+}
+
+// MARK: Conforming AddGeoRegionViewControllerDelegate
+extension GeoRegionsOnMapViewController: AddGeoRegionDelegate {
+    func addGeoRegionViewController(coordinate: CLLocationCoordinate2D, radius: Double, identifier: String, note: String, eventType: EventType) {
+        //can only monitor upto maximumRegionMonitoringDistance provided by location delegate
+        let maxDistance = min(radius, locationManager.maximumRegionMonitoringDistance)
+        let geoRegion = GeoRegion(coordinate: coordinate, radius: maxDistance, identifier: identifier, note: note, eventType: eventType)
+        self.addToGeoRegionArrayBasedOnEventType(geoRegion: geoRegion)
+        startMonitoring(geoRegion: geoRegion)
+        self.saveAllGeoRegions()
+    }
+}
+
+// MARK: Location Manager Delegate Methods
+extension GeoRegionsOnMapViewController: CLLocationManagerDelegate {
+    func startMonitoring(geoRegion: GeoRegion) {
+        if CLLocationManager.authorizationStatus() == .denied {
+            self.showAlert(title: "Warning", message: "This app requires access to Location. Please grant the Always Use permission in Settings")
+        }
+        else if CLLocationManager.authorizationStatus() != .authorizedAlways {
+            print("Always authorisation is required for region monitoring")
+            return
+        }
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+            let region = CLCircularRegion(center: geoRegion.coordinate, radius: geoRegion.radius, identifier: geoRegion.identifier)
+            region.notifyOnEntry = (geoRegion.eventType == .onEntry)
+            region.notifyOnExit = (geoRegion.eventType == .onExit)
+            locationManager.startMonitoring(for: region)
+        }
+    }
+    
+    func stopMonitoring(geoRegion: GeoRegion) {
+        for region in locationManager.monitoredRegions {
+            guard let circularRegion = region as? CLCircularRegion,
+                circularRegion.identifier == geoRegion.identifier else { continue }
+            locationManager.stopMonitoring(for: circularRegion)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?,withError error: Error) {
+        print("Monitoring failed for region with identifier: \(region!.identifier)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        if region is CLCircularRegion {
+            self.showAlert(title: "Notification", message: "Entered region with \(region.identifier)")
+            print("Entered:  Geofence triggered!")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        if region is CLCircularRegion {
+            self.showAlert(title: "Notification", message: "Exit from region with \(region.identifier)")
+            print("Exit:  Geofence triggered!")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            self.setupAfterAlwaysUsePermissionGrant()
+        }
+    }
+    
+    // MARK: Private location access methods
+    private func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            self.setupAfterAlwaysUsePermissionGrant()
+        } else {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    private func setupAfterAlwaysUsePermissionGrant() {
+        self.mapView.showsUserLocation = true
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+    }
+}
+
+// MARK: MapView Delegate Methods
+extension GeoRegionsOnMapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "myGeoRegion"
+        if annotation is GeoRegion { //To ensure it is not a user location annotation
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                let removeButton = UIButton()
+                removeButton.frame = CGRect(x: 0, y: 0, width: 23, height: 23)
+                removeButton.setImage(#imageLiteral(resourceName: "deleteImage"), for: .normal)
+                annotationView?.leftCalloutAccessoryView = removeButton
+            } else {
+                annotationView?.annotation = annotation
+            }
+            return annotationView
+        }
+        return nil
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let circleRenderer = MKCircleRenderer(overlay: overlay)
+            circleRenderer.lineWidth = 1.0
+            circleRenderer.strokeColor = .blue
+            circleRenderer.fillColor = UIColor.blue.withAlphaComponent(0.4)
+            return circleRenderer
+        }
+        return MKOverlayRenderer(overlay: overlay)
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let geoRegion = view.annotation as? GeoRegion {
+            stopMonitoring(geoRegion: geoRegion)
+            removeFromGeoRegionArrayBasedOnEventType(geoRegion: geoRegion)
+        }
+        saveAllGeoRegions()
+    }
+}
+
